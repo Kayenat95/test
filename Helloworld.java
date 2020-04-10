2d05e02566ba4b0ee956641228a6ba6f2f7450b7
Kayenat Aziz <kayenat95@gmail.com>
2020-04-10 22:42:01 +0530

    Testcommind with git

--- Helloworld.java
+++ Helloworld.java
@@ -4,6 +4,8 @@ class HelloWorld
       // Prints "Hello, World" to the terminal window.
       public static void main(String args[])
       {
-          System.out.println("Hello, World");
+          System.out.println("Test wih git polling in jenkins");
+          System.out.println("Second change! change done right");
+          System.out.println("Second change! change done right");
       }
   }
